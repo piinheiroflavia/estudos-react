22b3321c5a2ab769c86e0d6d5c17347c146c7243
Flavia Pinheio <flavia.p_17@hotmail.com>
2022-07-04 19:19:40 -0300

    primeiros passos com react

---
+++ primeiros-passos/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h4>Praticando os primeiros passos para implementar o react</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis accusamus nesciunt sunt accusantium assumenda, doloremque aliquam voluptates ducimus dolorem eos obcaecati sed libero iusto numquam commodi nemo, eius quaerat.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, omnis veniam ipsum ad officiis repellat illum ex illo reprehenderit minus a qui explicabo eos, quasi voluptas iure cum beatae! Laudantium.
+      </p>
+    </div>
+  );
+}
+
+export default App;
